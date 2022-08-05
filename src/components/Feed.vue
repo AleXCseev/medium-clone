452f7bebe9775e0c1c39c23fadf4a3570fc945a6
Kontantin Alekseev <alexcseev1990@gmail.com>
2022-08-05 17:07:41 +0300

    add pagination

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -26,7 +26,7 @@
                     TAG LIST
                 </router-link>
             </div>
-            PAGINATION
+            <app-pagination :total="feed.articlesCount" :limit="limit" :current-page="currentPage" :url="baseUrl"/>
         </div>
     </div>
 </template>
@@ -34,6 +34,10 @@
 <script>
 import { mapState } from "vuex"
 import { actionTypes } from "@/store/modules/feed"
+import AppPagination from "@/components/Pagination.vue"
+import { limit } from "@/helpers/vars"
+import { stringify, parseUrl } from "query-string"
+
 export default {
     name: "AppFeed",
     props: {
@@ -42,15 +46,49 @@ export default {
             required: true
         } 
     },
+    components: {
+        AppPagination
+    },
+    data() {
+        return {
+            limit,
+        }
+    },  
+    watch: {
+        currentPage() {
+            this.fetchFeed()
+        }
+    },
     mounted() {
-        this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl})
+        this.fetchFeed()
+    },
+    methods: {
+        fetchFeed() {
+            const parsedUrl = parseUrl(this.apiUrl)
+            const stringifiedParams = stringify({
+                limit,
+                offset: this.offset,
+                ...parsedUrl.query
+            })
+            const apiUrlWithParams = `${parsedUrl.url}?${stringifiedParams}`
+            this.$store.dispatch(actionTypes.getFeed, {apiUrl: apiUrlWithParams})
+        }
     },
     computed: {
         ...mapState({
             isLoading: state => state.feed.isLoading,
             feed: state => state.feed.data,
             error: state => state.feed.error,
-        })
+        }),
+        currentPage() {
+            return Number(this.$route.query.page || "1")
+        },
+        baseUrl() {
+            return this.$route.path
+        },
+        offset() {
+            return this.currentPage * limit - limit
+        }
     }
 }
 </script>
